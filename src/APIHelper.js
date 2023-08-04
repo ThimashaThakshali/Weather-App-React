f03d7d97de8d40304dfe2c19bb4a384c77db85ae
ThimashaThakshali <Thimasha.20210464@iit.ac.lk>
2023-08-04 14:21:14 +0530

    Added .env file

--- src/APIHelper.js
+++ src/APIHelper.js
@@ -1,7 +1,6 @@
-import appSettings from "./appSetting.json";
 export const API_BASE_URL = "https://api.openweathermap.org/data/2.5";
 export const API_ENDPOINTS = {
   GET_CITIES: "/group",
   GET_CITY: "/weather",
 };
-export const API_KEY = appSettings.apiKeys.openWeatherMapAPI;
+export const API_KEY = process.env.WEATHER_APP_API_KEY;
